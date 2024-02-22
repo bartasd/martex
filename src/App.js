57bb95acb2e64d23f637fd12164520ae200fbd6b
Bartas <bartas3@gmail.com>
2024-02-22 14:36:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import style from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col className={style.col} sm={8}>sm=8</Col>
+        <Col className={style.col} sm={4}>sm=4</Col>
+      </Row>
+      <Row>
+        <Col className={style.col} sm>sm=true</Col>
+        <Col className={style.col} sm>sm=true</Col>
+        <Col className={style.col} sm>sm=true</Col>
+      </Row>
+    </Container>
   );
 }
 
